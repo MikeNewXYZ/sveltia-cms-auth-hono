a0b3825a85570e93ff550ea376eeebf148009b9e
Mike Newberry <hello@mikenew.xyz>
2025-03-16 02:19:56 +0000

    feat: improved error messages in callback validators

--- packages/core/src/validators/callback-validator.ts
+++ packages/core/src/validators/callback-validator.ts
@@ -1,13 +1,12 @@
 import type { AppContext } from "@/types";
 import type { Context } from "hono";
 import { getCookie } from "hono/cookie";
-import { validator } from "hono/validator";
 import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
 
 const callbackQuerySchema = z.object({
-	code: z.string(),
-	state: z.string(),
+	code: z.string({ message: "Missing code query." }),
+	state: z.string({ message: "Missing state query." }),
 });
 
 export const callbackQueryValidator = zValidator(
@@ -27,17 +26,21 @@ export const callbackQueryValidator = zValidator(
 	},
 );
 
-export const callbackCookieValidator = validator("cookie", (_, c: Context<AppContext>) => {
-	const cookieState = getCookie(c, "cookie_state");
-	const provider = getCookie(c, "provider");
-
-	if (!cookieState) {
-		return c.text("Server responded with malformed data. Please try again later.", { status: 400 });
-	}
-
-	if (!provider) {
-		return c.text("Server responded with malformed data. Please try again later.", { status: 400 });
-	}
-
-	return { provider, cookieState };
+const callbackCookieSchema = z.object({
+	cookie_state: z.string({
+		message: "Missing cookie state cookie.",
+	}),
+	provider: z.string({
+		message: "Missing provider information cookie.",
+	}),
 });
+
+export const callbackCookieValidator = zValidator(
+	"cookie",
+	callbackCookieSchema,
+	(result, c: Context<AppContext>) => {
+		if (!result.success) {
+			return c.text(result.error.issues[0].message, { status: 422 });
+		}
+	},
+);
