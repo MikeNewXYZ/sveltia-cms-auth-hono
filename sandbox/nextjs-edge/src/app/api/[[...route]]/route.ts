0a4c04d7fbacca279dd89018418406b667614587
Mike Newberry <hello@mikenew.xyz>
2025-03-19 17:56:44 +0000

    fix: add GitHub authentication credentials to auth app configuration

--- sandbox/nextjs-edge/src/app/api/[[...route]]/route.ts
+++ sandbox/nextjs-edge/src/app/api/[[...route]]/route.ts
@@ -3,7 +3,14 @@ import { handleVercel } from "sveltia-cms-auth-hono/platforms/vercel";
 
 export const runtime = "edge";
 
-const app = createAuthApp();
+const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env;
+
+const app = createAuthApp({
+	authCredentials: {
+		githubClientId: GITHUB_CLIENT_ID as string,
+		githubClientSecret: GITHUB_CLIENT_SECRET as string,
+	},
+});
 
 export const GET = handleVercel(app);
 export const POST = handleVercel(app);
