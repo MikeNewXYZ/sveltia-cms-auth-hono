fd7d5c3c26484dbd34ebad7bb94081c5eb9574aa
Mike Newberry <hello@mikenew.xyz>
2025-03-22 15:33:27 +0000

    fix: update .gitignore to include dist directory and remove obsolete files from package

--- package/dist/types/lib/generate-oauth-callback-html.d.ts
+++
@@ -1,7 +0,0 @@
-import { Context } from 'hono';
-export type GenerateOAuthCallbackHTMLProps = {
-    c: Context;
-    provider?: string;
-    token: string;
-};
-export declare const generateOAuthCallbackHTML: ({ c, provider, token, }: GenerateOAuthCallbackHTMLProps) => Response;
