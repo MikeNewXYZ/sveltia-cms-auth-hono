e6f68b87a6b332140f46b47f853f3214d8d2f140
Mike Newberry <hello@mikenew.xyz>
2025-03-22 17:34:35 +0000

    feat: add nextjs-edge example

---
+++ sandbox/nextjs-edge/src/app/admin/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useEffect } from "react";
+import Script from "next/script";
+
+const AdminPage = () => {
+	useEffect(() => {
+		window.CMS_MANUAL_INIT = true;
+	}, []);
+
+	const initCMS = () => {
+		const { initCMS: init } = window;
+
+		init({
+			config: {
+				backend: {
+					name: "github",
+					branch: "main",
+					repo: "MikeNewXYZ/sveltia-cms-auth-hono",
+					base_url: "http://localhost:3000",
+					auth_endpoint: "api/auth",
+				},
+				media_folder: "sandbox/nextjs-edge/src/assets/",
+				public_folder: "sandbox/nextjs-edge/src/assets/",
+				collections: [
+					{
+						label: "Blogs",
+						name: "blogs",
+						folder: "sandbox/nextjs-edge/src/content/blogs",
+						create: true,
+						identifier_field: "title",
+						slug: "{{slug}}",
+						fields: [
+							{ label: "Title", name: "title", widget: "string" },
+							{ label: "Date", name: "date", widget: "datetime" },
+							{ label: "Body", name: "body", widget: "markdown" },
+						],
+					},
+				],
+			},
+		});
+	};
+
+	return (
+		<Script
+			src="https://unpkg.com/@sveltia/cms/dist/sveltia-cms.js"
+			onLoad={initCMS}
+			onError={() => console.error("Failed to load CMS script")}
+		/>
+	);
+};
+
+export default AdminPage;
---
+++ sandbox/nextjs-edge/src/app/api/[[...route]]/route.ts
@@ -0,0 +1,22 @@
+import { Hono } from "hono/tiny";
+import { handle } from "hono/vercel";
+import { createAuthApp } from "sveltia-cms-auth-hono";
+
+export const runtime = "edge";
+
+const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env;
+
+const app = new Hono().route(
+	"/api",
+	createAuthApp({
+		provider: {
+			github: {
+				clientId: GITHUB_CLIENT_ID as string,
+				clientSecret: GITHUB_CLIENT_SECRET as string,
+			},
+		},
+	}),
+);
+
+export const GET = handle(app);
+export const POST = handle(app);
